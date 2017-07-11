4b581baa6863a6d5c78a6dba91611ebacdc24056
zhuxinye12321 <zhuxinye0517@foxmail.com>
2017-07-11 18:10:08 +0800

    增加了实体类

---
+++ src/main/java/com/springboot/domain/TpPersonal.java
@@ -0,0 +1,24 @@
+package com.springboot.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by Administrator on 2017/7/11.
+ */
+@Entity
+@Data
+public class TpPersonal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String realName;
+    private String userName;
+    private String password;
+    private Integer phoneNumber;
+    private String email;
+}
---
+++ src/main/java/com/springboot/domain/TpServiceProvider.java
@@ -0,0 +1,23 @@
+package com.springboot.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Created by Administrator on 2017/7/11.
+ */
+
+@Entity
+@Data
+public class TpServiceProvider {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String city;
+    private Integer userId;
+    private Integer enterpriseId;
+
+}

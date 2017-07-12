47d79169d0153e8ddb45c88fb9e13429ce155e33
zhuxinye12321 <zhuxinye0517@foxmail.com>
2017-07-12 10:14:47 +0800

    增加了controller、service

---
+++ src/main/java/com/springboot/controller/PersonalController.java
@@ -0,0 +1,10 @@
+package com.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Administrator on 2017/7/11.
+ */
+@Controller
+public class PersonalController {
+}
---
+++ src/main/java/com/springboot/service/impl/ServiceProviderServiceImpl.java
@@ -0,0 +1,12 @@
+package com.springboot.service.impl;
+
+
+import com.springboot.service.ServiceProviderService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Administrator on 2017/7/12.
+ */
+@Service
+public class ServiceProviderServiceImpl implements ServiceProviderService {
+}

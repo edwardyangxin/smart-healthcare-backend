1ca2e486272bd16a537eaca2da74a4bd0cf4a71f
zhuxinye12321 <zhuxinye0517@foxmail.com>
2017-07-17 16:40:21 +0800

    个人用户修改密码

--- src/main/java/com/springboot/service/impl/PersonalServiceImpl.java
+++ src/main/java/com/springboot/service/impl/PersonalServiceImpl.java
@@ -26,7 +26,7 @@ public class PersonalServiceImpl implements PersonalService {
         //获取成功后，将获取用户的密码和传入密码对比
         else if (!tpPersonal.getPassword().equals(password)) {
             return "密码错误";
-        }else {
+        } else {
             return "登录成功";
         }
     }
@@ -40,27 +40,27 @@ public class PersonalServiceImpl implements PersonalService {
     @Override
     public String insertPerson(TpPersonal person) {
         TpPersonal person1 = personalMapper.selectByName(person.getName());
-        if(person1==null){
+        if (person1 == null) {
             personalMapper.insertPerson(person);
             return "注册成功！";
-        }else{
-            return "用户"+person1.getName()+"已存在！";
+        } else {
+            return "用户" + person1.getName() + "已存在！";
         }
     }
 
     @Override
-    public String updatePersonalPass(String name, String password, String newPassword, String retypePassword, TpPersonal tpPersonal){
-        tpPersonal= personalMapper.selectByName(name);
-        String TPPassword= tpPersonal.getPassword();
-        if (password.equals(TPPassword)){
-            if (newPassword.equals(retypePassword)){
+    public String updatePersonalPass(String name, String password, String newPassword, String retypePassword, TpPersonal tpPersonal) {
+        tpPersonal = personalMapper.selectByName(name);
+        String TPPassword = tpPersonal.getPassword();
+        if (password.equals(TPPassword)) {
+            if (newPassword.equals(retypePassword)) {
                 tpPersonal.setPassword(newPassword);
                 personalMapper.updatePassword(tpPersonal);
                 return "密码修改成功！";
-            }else {
+            } else {
                 return "两次输入的新密码不同，请重试！";
             }
-        }else {
+        } else {
             return "密码输入错误，请重试！";
         }
     }

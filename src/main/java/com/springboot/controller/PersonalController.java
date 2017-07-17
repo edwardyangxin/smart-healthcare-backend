1c75548b908301bf64ab94e37c84dc74c7b32fcc
liuyongguang <1363186473@qq.com>
2017-07-17 17:05:58 +0800

    dto

--- src/main/java/com/springboot/controller/PersonalController.java
+++ src/main/java/com/springboot/controller/PersonalController.java
@@ -15,7 +15,6 @@ import javax.validation.Valid;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import java.util.List;
 
 /**

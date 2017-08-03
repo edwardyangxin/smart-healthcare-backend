1834ac360729ff1a67fde5fecfe95de3a18480a6
13368340559 <xue.gu@hpe.com>
2017-08-03 14:21:24 +0800

    企业项目信息发布、企业信息查询、企业信息查询前十条

--- src/main/java/com/springboot/service/EnterpriseService.java
+++ src/main/java/com/springboot/service/EnterpriseService.java
@@ -1,8 +1,13 @@
 package com.springboot.service;
 
 import com.springboot.domain.TpEnterprise;
+import com.springboot.domain.TpEnterpriseProject;
 import com.springboot.dto.Enterprise;
+import com.springboot.dto.EnterpriseResetPass;
 import com.springboot.dto.Password;
+import com.springboot.dto.SelectEnterpriseProject;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/7/12.
@@ -14,5 +19,8 @@ public interface EnterpriseService {
     String insertEnterprise(TpEnterprise enterprise);
     String updateEnterpriseByName(Enterprise enterprise);
     String updateEnterprisePassByName(Password password);
-
+    String resetEnterprisePass(EnterpriseResetPass enterpriseResetPass);
+    String newProject(TpEnterpriseProject tpEnterpriseProject);
+    List<TpEnterpriseProject> selectProjects(SelectEnterpriseProject selectEnterpriseProject);
+    List<TpEnterpriseProject> selectLatest();
 }

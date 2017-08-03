aba9e38b8d4562a5fecb794cee6250da016c5c92
zhuxinye12321 <zhuxinye0517@foxmail.com>
2017-08-03 10:49:55 +0800

    8、3

--- src/main/java/com/springboot/controller/PersonalController.java
+++ src/main/java/com/springboot/controller/PersonalController.java
@@ -2,10 +2,7 @@ package com.springboot.controller;
 
 import com.springboot.domain.TpPersonInfo;
 import com.springboot.domain.TpPersonal;
-import com.springboot.dto.Password;
-import com.springboot.dto.Personal;
-import com.springboot.dto.PersonalResetPass;
-import com.springboot.dto.SelectPersonInfo;
+import com.springboot.dto.*;
 import com.springboot.service.PersonalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,8 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 
@@ -38,12 +33,18 @@ public class PersonalController {
     //个人登陆
     @ResponseBody
     @PostMapping(value = "/personal/login")
-    public String personalLogin(String name, String password, HttpSession session) {
-        String result = personalService.login(name, password);
+    public String personalLogin(@Valid @RequestBody Login login, BindingResult bindingResult, HttpSession session) {
+        if (bindingResult.hasErrors()) {
+            List<ObjectError> errorList = bindingResult.getAllErrors();
+            for (ObjectError error : errorList) {
+                return error.getDefaultMessage();
+            }
+        }
+        String result = personalService.login(login);
         if (result.equals("登录成功")) {
             //添加用户信息到session中
-            TpPersonal tpPersonal = personalService.selectByName(name);
-            session.setAttribute("name", name);
+            TpPersonal tpPersonal = personalService.selectByName(login.getName());
+            session.setAttribute("name", login.getName());
         }
         return result;
     }
@@ -51,7 +52,7 @@ public class PersonalController {
     //个人注册
     @ResponseBody
     @RequestMapping(value = "/personal/register", method = RequestMethod.POST)
-    public String insertPerson(@Valid TpPersonal person, BindingResult bindingResult) {
+    public String insertPerson(@Valid @RequestBody TpPersonal person, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             List<ObjectError> errorList = bindingResult.getAllErrors();
             for (ObjectError error : errorList) {
@@ -64,7 +65,7 @@ public class PersonalController {
     //个人密码修改
     @ResponseBody
     @PostMapping(value = "/personal/modifyPass")
-    public String modifyPass(@Valid Password password, BindingResult bindingResult, HttpSession session) {
+    public String modifyPass(@Valid @RequestBody Password password, BindingResult bindingResult, HttpSession session) {
         if (bindingResult.hasErrors()) {
             List<ObjectError> errorList = bindingResult.getAllErrors();
             for (ObjectError error : errorList) {
@@ -79,7 +80,7 @@ public class PersonalController {
     //个人重置密码（真实姓名、邮箱、电话）
     @ResponseBody
     @PostMapping(value = "/personal/resetPass")
-    public String resetPass(@Valid PersonalResetPass personalResetPass, BindingResult bindingResult) {
+    public String resetPass(@Valid @RequestBody PersonalResetPass personalResetPass, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             List<ObjectError> errorList = bindingResult.getAllErrors();
             for (ObjectError error : errorList) {
@@ -93,7 +94,7 @@ public class PersonalController {
     //个人信息修改
     @ResponseBody
     @RequestMapping(value = "/personal/modifyPerson", method = RequestMethod.POST)
-    public String modifyPerson(@Valid Personal person, BindingResult bindingResult, HttpSession session) {
+    public String modifyPerson(@Valid @RequestBody Personal person, BindingResult bindingResult, HttpSession session) {
         if (bindingResult.hasErrors()) {
             List<ObjectError> errorList = bindingResult.getAllErrors();
             for (ObjectError error : errorList) {
@@ -104,10 +105,10 @@ public class PersonalController {
         return personalService.updatePersonByName(person);
     }
 
-    //发布个人信息
+    //发布个人信息、、接受Json格式的参数 Content-Type:application/json
     @ResponseBody
     @PostMapping(value = "/personal/newInfo")
-    public String newInfo(TpPersonInfo tpPersonInfo) {
+    public String newInfo(@RequestBody TpPersonInfo tpPersonInfo) {
         tpPersonInfo.setRegisterTime(new Date());
         return personalService.newInfo(tpPersonInfo);
     }
@@ -115,7 +116,7 @@ public class PersonalController {
     //查询个人发布的信息,可以单条件查询，也可以多条件组合查询
     @ResponseBody
     @PostMapping(value = "/personal/selectInfo")
-    public List<TpPersonInfo> selectInfos(SelectPersonInfo selectPersonInfo) {
+    public List<TpPersonInfo> selectInfos(@RequestBody SelectPersonInfo selectPersonInfo) {
         List<TpPersonInfo> tpPersonInfos = personalService.selectInfos(selectPersonInfo);
         return tpPersonInfos;
     }

85c8f1855be646367a278299cc542a2ce0d188a5
13368340559 <xue.gu@hpe.com>
2017-08-07 10:32:12 +0800

    企业项目信息发布、企业信息查询、企业信息查询前十条

--- src/main/java/com/springboot/dto/Password.java
+++ src/main/java/com/springboot/dto/Password.java
@@ -23,38 +23,6 @@ public class Password {
     @Size(min = 6, max = 16, message="新密码长度必须在6到16之间！")
     private String newPassword;
 
-    public String getName() {
-        return name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getNewPassword() {
-        return newPassword;
-    }
-
-    public String getRetypePassword() {
-        return retypePassword;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setNewPassword(String newPassword) {
-        this.newPassword = newPassword;
-    }
-
-    public void setRetypePassword(String retypePassword) {
-        this.retypePassword = retypePassword;
-    }
-
     @NotEmpty(message = "再次输入的新密码不能为空！")
     @NotNull(message = "再次输入的新密码不能为空！")
     @Size(min = 6, max = 16, message="再次输入的新密码长度必须在6到16之间！")

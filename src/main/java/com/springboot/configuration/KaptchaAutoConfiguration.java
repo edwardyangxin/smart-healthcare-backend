5b7570c9c6c0871580a804cf500ede7d0e399e7f
pangx <xlong010@163.com>
2017-09-01 18:17:38 +0800

    嘻嘻哈哈

--- src/main/java/com/springboot/configuration/KaptchaAutoConfiguration.java
+++ src/main/java/com/springboot/configuration/KaptchaAutoConfiguration.java
@@ -25,7 +25,6 @@ public class KaptchaAutoConfiguration {
     public DefaultKaptcha getKaptchaBean() {
 
         DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
-
         Properties properties = new Properties();
         properties.setProperty(Constants.KAPTCHA_BORDER, kaptchaProperties.getBorder());
         properties.setProperty(Constants.KAPTCHA_BORDER_COLOR, kaptchaProperties.getBorderColor());

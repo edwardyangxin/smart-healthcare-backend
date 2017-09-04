6fe54fba89da7db120e4ac6fe4252a001cb5e0d1
liuyongguang <1363186473@qq.com>
2017-09-04 18:11:01 +0800

    供应商删除发布的个人和企业消息，密码修改，密码重置。

--- src/main/java/com/springboot/dto/Enterprise.java
+++ src/main/java/com/springboot/dto/Enterprise.java
@@ -3,11 +3,9 @@ package com.springboot.dto;
 import lombok.Data;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
-import org.springframework.format.annotation.NumberFormat;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
 /**
  * Created by liuyongg on 18/7/2017.
@@ -19,7 +17,6 @@ public class Enterprise {
 
     @Pattern(regexp = "^1[34578]\\d{9}$", message = "手机号码格式不正确！")
     private String tel;
-
     @NotEmpty(message = "邮箱地址不能为空！")
     @NotNull(message = "邮箱地址不能为空！")
     @Email(message = "邮箱地址不符合规范！")
